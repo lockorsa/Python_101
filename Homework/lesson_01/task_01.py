fc5a2b6bffa4ea20124845af6d255f2d1c06d280
lockorsa <71004031+lockorsa@users.noreply.github.com>
2021-01-11 11:33:54 +0700

    Completed HW

---
+++ Homework/lesson_01/task_01.py
@@ -0,0 +1,17 @@
+"""
+1. Поработайте с переменными, создайте несколько, выведите на экран, запросите у пользователя несколько чисел и строк и
+сохраните в переменные, выведите на экран.
+"""
+
+first_variable = "Hello world"  # string строка
+second_variable = "Hi \'GB"
+
+user_prompt = input
+
+print(first_variable)
+print(second_variable)
+
+user_prompt = input('Как дела? \n >>>')
+print('Ага, как скажешь')
+
+# демдальш
